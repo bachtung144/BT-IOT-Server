781a2ab59f5c45594ed37855e86f128f39aa3cd3
Nguyễn Bách Tùng <tungnb_tt@rikkeisoft.com>
2021-04-27 15:07:45 +0700

    add topic

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ app.use(bodyParser.json());
 
 client.on('connect', () => {
     client.subscribe('list')
-
-    client.publish('list', JSON.stringify([1,2,3,4]))
+    client.subscribe('state')
+    client.publish('list', JSON.stringify([1,2,3,4,5]))
 })
+
+client.on('message', (topic, message) => {
+    switch (topic) {
+        case 'state':
+            return handleList(message)
+
+    }
+    console.log('No handler for topic %s', topic)
+})
+
+const handleList = (message) => {
+    console.log('mess from',message.toString())
+    if (message.toString() === 'listDevice') {
+        client.publish('list', JSON.stringify([1,2,3,4]))
+    }
+}
+
+
 
 app.use(function(err, req, res, next){
     res.status(422).send({error: err.message});
